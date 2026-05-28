157554d8c2f2d866ce442f349dd45acadfd11ab1
ParasParmar2111981335 <parasparmar7373@gmail.com>
2026-05-28 15:48:29 +0530

    Wipro_PracticeDay30_Cucumber
    Wipro_PracticeDay30_Cucumber

---
+++ parmar/BaseClass.java
@@ -0,0 +1,5 @@
+package parmar;
+import org.openqa.selenium.WebDriver;
+public class BaseClass {
+	public static WebDriver driver;
+}
---
+++ stepdefinition/Loginsteps.java
@@ -0,0 +1,49 @@
+package stepdefinition;
+import parmar.BaseClass;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+public class Loginsteps extends BaseClass{
+	@Given("user launches browser")
+    public void launch_browser()
+    {
+        System.out.println("Browser launched successfully");
+    }
+
+    @Given("user opens login page")
+    public void open_login_page()
+    {
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+        System.out.println("Application opened");
+    }
+
+    @When("user enter username {string}")
+    public void enter_username(String username)
+    {
+        System.out.println("Username: " + username);
+    }
+
+    @When("user enter password {string}")
+    public void enter_password(String password)
+    {
+        System.out.println("Password: " + password);
+    }
+
+    @When("clicks on login button")
+    public void enter_login()
+    {
+        System.out.println("Clicked login button");
+    }
+
+    @Then("user should navigate to dashboard")
+    public void dashboard()
+    {
+        System.out.println("Dashboard Displayed");
+    }
+
+    @Then("error message should display")
+    public void error_message()
+    {
+        System.out.println("Invalid credentials");
+    }
+}
